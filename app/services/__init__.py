7aa4bc08128c9ef99cf5599218db65e3630f5357
Sergio <santossergiorealbjj@outlook.com>
2025-09-21 18:07:50 -0300

    Create __init__.py

---
+++ app/services/__init__.py
@@ -0,0 +1,4 @@
+# Este arquivo transforma o diretório 'services' em um subpacote Python.
+# Ele permite que a lógica de análise de segurança em 'stride_analyzer.py'
+# seja importada pelo arquivo 'api.py'.
+
